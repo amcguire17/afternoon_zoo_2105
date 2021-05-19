fba23477252eac085c1c1f1b789c033e58993dcc
Amanda McGuire <aruwart001@gmail.com>
2021-05-19 14:37:52 -0600

    update zoo spec file for iteration 3 tests

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -3,6 +3,8 @@ require './lib/animal'
 require './lib/zoo'
 
 RSpec.describe Zoo do
+
+  # Iteration 2
   it 'exists' do
     zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
 
@@ -73,4 +75,79 @@ RSpec.describe Zoo do
     expect(zoo.animal_count).to eq(2)
   end
 
+  # Iteration 3
+  it 'has animals older than 250 weeks' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    animal_3 = Animal.new("Capybara", 100, 150)
+    animal_4 = Animal.new("Dolphin", 150, 200)
+
+    zoo.add_animal(animal_1)
+    zoo.add_animal(animal_2)
+    zoo.add_animal(animal_3)
+    zoo.add_animal(animal_4)
+
+    expect(zoo.animals_older_than(250)).to eq([])
+  end
+
+  it 'has animals older than 100 weeks' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    animal_3 = Animal.new("Capybara", 100, 150)
+    animal_4 = Animal.new("Dolphin", 150, 200)
+
+    zoo.add_animal(animal_1)
+    zoo.add_animal(animal_2)
+    zoo.add_animal(animal_3)
+    zoo.add_animal(animal_4)
+
+    expect(zoo.animals_older_than(100)).to eq([animal_3, animal_4])
+  end
+
+  it 'has animals older than 10 weeks' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    animal_3 = Animal.new("Capybara", 100, 150)
+    animal_4 = Animal.new("Dolphin", 150, 200)
+
+    zoo.add_animal(animal_1)
+    zoo.add_animal(animal_2)
+    zoo.add_animal(animal_3)
+    zoo.add_animal(animal_4)
+
+    expect(zoo.animals_older_than(10)).to eq([animal_1, animal_2, animal_3, animal_4])
+  end
+
+  it 'has a total weight of animals' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    animal_3 = Animal.new("Capybara", 100, 150)
+    animal_4 = Animal.new("Dolphin", 150, 200)
+
+    zoo.add_animal(animal_1)
+    zoo.add_animal(animal_2)
+    zoo.add_animal(animal_3)
+    zoo.add_animal(animal_4)
+
+    expect(zoo.total_weight_of_animals).to eq(265)
+  end
+
+  it 'has details of zoo' do
+    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    animal_3 = Animal.new("Capybara", 100, 150)
+    animal_4 = Animal.new("Dolphin", 150, 200)
+
+    zoo.add_animal(animal_1)
+    zoo.add_animal(animal_2)
+    zoo.add_animal(animal_3)
+    zoo.add_animal(animal_4)
+
+    expect(zoo.details).to eq({"total_weight" => 265, "street_address" => "2300 Southern Blvd"})
+  end
 end
